4f82b583b3fb4bc4ea384dfbf9c7137b13febcaa
gangjiang <liksyunf76@gmail.com>
2023-05-16 22:24:00 +0800

    update: 更新配置文件

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yuanwenq/nodejs_dev_stack.git main:gh-pages
+git push -f git@github.com:yuanwenq/nodejs_dev_stack.git master:gh-pages
 
 cd -
